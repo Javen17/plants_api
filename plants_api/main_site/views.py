73b6b70933cac4a441341406653716b5100a9e2e
Javier <javieroviedo2000@gmail.com>
2019-10-19 22:47:17 -0600

    added search in species

--- plants_api/main_site/views.py
+++ plants_api/main_site/views.py
@@ -6,6 +6,7 @@ from .serializers import PlantSpeciesSerializer, PlantFamilySerializer , PlantSp
 from django.http import HttpResponse , JsonResponse
 from rest_framework.renderers import JSONRenderer
 from django.db.models import Q
+from rest_framework.parsers import FileUploadParser
 #from rest_framework.generics import ListCreateAPIView , RetrieveUpdateDestroyAPIView
 class PlantFamilyViewSet(viewsets.ModelViewSet):
     """
@@ -17,6 +18,7 @@ class PlantFamilyViewSet(viewsets.ModelViewSet):
 class PlantSpeciesViewSet(viewsets.ModelViewSet):
     serializer_class = PlantSpeciesSerializer
     queryset = PlantSpecies.objects.all()
+    parser_class = (FileUploadParser,)
 
     def perform_create(self, serializer):
 
@@ -26,15 +28,9 @@ class PlantSpeciesViewSet(viewsets.ModelViewSet):
             family_name = family_data["family_name"]
 
             if family_name != '':
-                family_list = PlantFamily.objects.filter(
-                    family_name=family_name)
 
-                if not family_list:
-                    new_family = PlantFamily(family_name = family_name)
-                    family = new_family
-                    new_family.save()
-                else:
-                    family = family_list[0]
+                family = get_or_created_family(family_data)
+
 
                 model_instance = PlantSpecies(common_name = serializer.validated_data.get("common_name") ,
                     scientific_name = serializer.validated_data.get("scientific_name"),
@@ -50,6 +46,15 @@ class PlantSpeciesViewSet(viewsets.ModelViewSet):
         else:
             return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @action(methods=["get"] , detail = False)
+    def search_species(self , request , pk = None):
+        common_name = self.request.query_params.get('common_name',None)
+        result = search(self.queryset , "common_name__icontains" , common_name , PlantSpeciesSerializer)
+
+        json = JSONRenderer().render(result)
+        return HttpResponse(json)
+
+
 class RecolectorViewSet(viewsets.ModelViewSet):
     serializer_class = RecolectorSerializer
     queryset = Recolector.objects.all()
@@ -57,9 +62,7 @@ class RecolectorViewSet(viewsets.ModelViewSet):
     @action(methods=['get'], detail=False)
     def search_recolector(self, request, pk=None):
         name = self.request.query_params.get('name', None)
-        result = search(self.queryset , "name__icontains" , name)
-
-        print(result)
+        result = search(self.queryset , "name__icontains" , name , RecolectorSerializer )
 
         json = JSONRenderer().render(result)
         return HttpResponse(json)
@@ -69,13 +72,20 @@ class SpecimenStatusViewSet(viewsets.ModelViewSet):
     serializer_class = SpecimenStatusSerializer
     queryset = SpecimenStatus
 
+
 class PlantSpecimenViewSet(viewsets.ModelViewSet):
     serializer_class = PlantSpecimenSerializer
     queryset = PlantSpecimen.objects.all()
 
+    @action(methods=['get'], detail=False)
+    def search_specimen(self, request, pk=None):
+        plant_species = self.request.query_params.get('plant_species', None)
+        result = search(self.queryset , "plant_species__common_name__icontains" , plant_species , PlantSpecimenSerializer)
+        json = JSONRenderer().render(result)
+        return HttpResponse(json)
 
 
-def search(queryset , filter ,  search_field):
+def search(queryset , filter ,  search_field , serializer):
 
     if search_field is not None:
         q_objects = Q()
@@ -85,9 +95,26 @@ def search(queryset , filter ,  search_field):
         resultdict = {}
 
         for found in found_elements:
-            serializer = RecolectorSerializer(found)
+            serializer = serializer(found)
             resultdict.update(serializer.data)
 
         return resultdict
     else:
         return  "please provide a valid search url"
+
+
+def get_or_created_family(family_data):
+    family_name = family_data["family_name"]
+
+
+    family_list = PlantFamily.objects.filter(
+        family_name=family_name)
+
+    if not family_list:
+        new_family = PlantFamily(family_name = family_name)
+        family = new_family
+        new_family.save()
+    else:
+        family = family_list[0]
+
+    return family
