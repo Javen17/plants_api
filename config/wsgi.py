dedc8642f217f789c2cffc8f70533512e39a37c3
Javier <javieroviedo2000@gmail.com>
2019-10-20 15:15:42 -0600

    fixing whitenoise configuration changes i v4

--- config/wsgi.py
+++ config/wsgi.py
@@ -34,7 +34,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
